b6637137b04b6c88f71bfcd9b3a5c7dc5c7ea4cc
Faradey <ikg1366@ya.ru>
2023-09-11 20:24:24 +0300

    test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,16 +1,20 @@
 import {fireEvent, render, screen} from "@testing-library/react";
-import {Sidebar} from "widgets/Sidebar";
+
 import {withTranslation} from "react-i18next";
+
+import {Sidebar} from "widgets/Sidebar";
 import {componentRender} from "shared/lib/tests/componentRender/componentRender";
 
 
 describe('sidebar', ()=> {
     test('existence', () => {
+
         const SidebarWithTranslation = withTranslation()(Sidebar)
         componentRender(<SidebarWithTranslation />)
         expect(screen.getByTestId('sidebar')).toBeInTheDocument()
     })
     test('toggle button', () => {
+
         componentRender(<Sidebar />)
         expect(screen.getByTestId('sidebar')).toBeInTheDocument()
         const toggleBtn = screen.getByTestId('toggle-sidebar');
