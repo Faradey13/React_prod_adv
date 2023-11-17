67173f3eafefbf7066f06cfb324d39c5778db1a2
Faradey <ikg1366@ya.ru>
2023-11-17 10:00:41 +0300

    fix type & storybook new build

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -1,11 +1,11 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { themeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { Themes } from "app/providers/ThemeProvider";
-import {Navbar} from "widgets/Navbar";
-
+import { Navbar } from "widgets/Navbar";
+import { StoreDecorator } from "shared/config/storybook/StoreDecorator/StoreDecorator";
 
 const meta: Meta<typeof Navbar> = {
-    title: 'widget/Navbar',
+    title: "widget/Navbar",
     component: Navbar,
 };
 
@@ -14,8 +14,15 @@ export default meta;
 type Story = StoryObj<typeof Navbar>;
 
 export const Light: Story = {
-    args: {},
+};
+export const LightNoAuth: Story = {
 };
 export const Dark: Story = {
-    decorators : [themeDecorator(Themes.DARK)]
 };
+export const DarkNoAuth: Story = {
+};
+
+Light.decorators = [StoreDecorator({user: {authData: {username:'bob'}}})]
+Dark.decorators = [StoreDecorator({user: {authData: {username:'bob'}}}), themeDecorator(Themes.DARK)]
+LightNoAuth.decorators = [StoreDecorator({user:{}})]
+DarkNoAuth.decorators = [StoreDecorator({user:{}}), themeDecorator(Themes.DARK)]
